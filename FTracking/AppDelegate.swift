8954588f6ddc995568f06884d594e7d7bd2efaa9
lj <lj@ljs-MacBook-Pro.local>
2014-12-05 19:42:05 +1100

    Initial Commit

---
+++ FTracking/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FTracking
+//
+//  Created by lj on 5/12/2014.
+//  Copyright (c) 2014 uws. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
